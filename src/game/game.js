cd174753bd30574bf0e6f19d48f2eb0342573f91
Tom <tomoliverallen@gmail.com>
2023-02-04 14:45:17 +0000

    draw detection and grid refactor

--- src/game/game.js
+++ src/game/game.js
@@ -1,33 +1,49 @@
 class Game {
   constructor() {
-    this.grid = [[null, null, null],[null, null, null],[null, null, null]]
-    this.symbol = 'X'
+    this.grid = [
+      null, null, null,
+      null, null, null,
+      null, null, null,
+    ];
+    this.symbol = "X";
   }
 
   addSymbol(coords) {
-    if (this.grid[coords[0]][coords[1]] != null) {throw new Error("Error, can't take turn in same position"); 
-  } else {
-    this.grid[coords[0]][coords[1]] = this.symbol
-    return this.grid}
+    if (this.grid[coords] != null) {
+      throw new Error("Error, can't take turn in same position");
+    } else {
+      this.grid[coords] = this.symbol;
+      return this.grid;
+    }
   }
 
   changeSymbol(currentSymbol) {
-    if (currentSymbol === 'O') {
-      this.symbol = 'X'
-    } else {
-      this.symbol = 'O'
-    }
-  } 
+    currentSymbol === "O" ? (this.symbol = "X") : (this.symbol = "O")
+  }
 
   takeTurn(coords) {
-    this.changeSymbol(this.symbol)
-    this.addSymbol(coords)
+    this.changeSymbol(this.symbol);
+    this.addSymbol(coords);
   }
 
   endGame() {
+
+    const winningConditions = [
+      [0, 1, 2],  //horizontal
+      [3, 4, 5],  //horizontal
+      [6, 7, 8],  //horizontal
+      [0, 3, 6],  //vert
+      [1, 4, 7],  //vert
+      [2, 5, 8],  //vert
+      [0, 4, 8],  //diagonal
+      [2, 4, 6]   //diagonal
+  ];
+
+
     if (this.grid.indexOf(null) === -1) {
-    return 'Draw!' }
+      return "Draw!";
+    } else { return "BLAH"}
   }
-} 
+}
 
 module.exports = Game;
--- src/test/game.test.js
+++ src/test/game.test.js
@@ -1,71 +1,123 @@
-const Game = require('../game/game');
+const Game = require("../game/game");
 
-describe('Game', () => {
-  describe('Turn one', () => {
-    it('Adds a nought to the grid top left', () => {
+describe("Game", () => {
+  describe("Turn one", () => {
+    it("Adds a nought to the grid top left", () => {
       const game = new Game();
-      game.addSymbol([0,0]);
+      game.addSymbol([0]);
 
-      expect(game.grid).toEqual([['X', null, null],[null, null, null],[null, null, null]]);
-    })
+      expect(game.grid).toEqual(
+        ["X", null, null,
+        null, null, null,
+        null, null, null]
+      );
+    });
 
-    it('Adds a cross to the grid bottom left', () => {
+    it("Adds a cross to the grid bottom left", () => {
       const game = new Game();
-      game.addSymbol([2,0]);
+      game.addSymbol([6]);
 
-      expect(game.grid).toEqual([[null, null, null],[null, null, null],['X', null, null]]);
-    })
-  })
+      expect(game.grid).toEqual(
+        [null, null, null,
+        null, null, null,
+        "X", null, null]
+      );
+    });
+  });
 
-  describe('Turn two', () => {
-    it('Doesn`t allow another turn in same grid position', () => {
+  describe("Turn two", () => {
+    it("Doesn`t allow another turn in same grid position", () => {
       const game = new Game();
 
-      game.addSymbol([0,0]);
+      game.addSymbol([0]);
 
-      expect(() => game.addSymbol([0,0])).toThrow(Error)
-    })
-  })
+      expect(() => game.addSymbol([0])).toThrow(Error);
+    });
+  });
 
-  describe('Automatically switch between O & X', () => {
-    it('changes after each turn', () => {
+  describe("Automatically switch between O & X", () => {
+    it("changes after each turn", () => {
       const game = new Game();
-      game.takeTurn([0,0]);
-      game.takeTurn([2,0]);
-      game.takeTurn([2,2]);
+      game.takeTurn([0]);
+      game.takeTurn([6]);
+      game.takeTurn([8]);
 
-      expect(game.grid).toEqual([['O', null, null],[null, null, null],['X', null, 'O']]);
-    })
+      expect(game.grid).toEqual([
+        "O", null, null,
+        null, null, null,
+        "X", null, "O"
+      ]);
+    });
 
-    it('allows a player to have a second attempt after an illegal move', () => {
+    it("allows a player to have a second attempt after an illegal move", () => {
       const game = new Game();
-      game.takeTurn([0,0]);
-      expect(() => game.taketurn([0,0])).toThrow(Error)
-      game.takeTurn([0,1]);
+      game.takeTurn([0]);
+      expect(() => game.taketurn([0])).toThrow(Error);
+      game.takeTurn([1]);
 
-  
+      expect(game.grid).toEqual([
+        "O", "X", null,
+        null, null, null,
+        null, null, null
+      ]);
+    });
+  });
 
-      expect(game.grid).toEqual([['O', 'X', null],[null, null, null],[null, null, null]]);
-    })
-  })
-
-  describe('Game ends', () => {
-    describe('when grid is full', () => {
-      it('announces a draw', () => {
+  describe("Game ends", () => {
+    describe("when grid is full", () => {
+      it("announces a draw", () => {
         const game = new Game();
-        game.takeTurn([0,0]);
-        game.takeTurn([0,1]);
-        game.takeTurn([0,2]);
-        game.takeTurn([1,0]);
-        game.takeTurn([1,1]);
-        game.takeTurn([2,0]);
-        game.takeTurn([1,2]);
-        game.takeTurn([2,2]);
-        game.takeTurn([2,1]);
-      
-        expect(game.grid).toEqual([['O', 'X', 'O'],['X', 'O', 'O'],['X', 'O', 'X']]);
-        expect(game.endGame()).toEqual('Draw!')
-      })
-      })
-    })
-  })
+        game.takeTurn([0]);
+        game.takeTurn([1]);
+        game.takeTurn([2]);
+        game.takeTurn([3]);
+        game.takeTurn([4]);
+        game.takeTurn([6]);
+        game.takeTurn([5]);
+        game.takeTurn([8]);
+        game.takeTurn([7]);
+
+        expect(game.grid).toEqual([
+          "O", "X", "O",
+          "X", "O", "O",
+          "X", "O", "X"
+        ]);
+        console.log(game.grid);
+        expect(game.endGame()).toEqual("Draw!");
+      });
+    });
+
+      it("doesn't announce a draw if <9 moves", () => {
+        const game = new Game();
+        game.takeTurn([0]);
+        game.takeTurn([1]);
+        game.takeTurn([2]);
+        game.takeTurn([3]);
+        game.takeTurn([4]);
+        game.takeTurn([6]);
+        game.takeTurn([5]);
+        game.takeTurn([8]);
+
+        expect(game.grid).toEqual([
+          "O", "X", "O",
+          "X", "O", "O",
+          "X", null, "X"
+        ]);
+        console.log(game.grid);
+        expect(game.endGame()).not.toEqual("Draw!");
+      });
+
+    describe("when a player wins (horizontally)", () => {
+      it("announces a win and stops play", () => {
+        const game = new Game();
+        game.takeTurn([0]);
+        game.takeTurn([3]);
+        game.takeTurn([1]);
+        game.takeTurn([4]);
+        game.takeTurn([2]);
+
+        expect(game.endGame()).toEqual("O wins!");
+      });
+    });
+  });
+});
